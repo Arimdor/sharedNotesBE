568ad830b7680e5fb85d136952ebfe578c94db2a
IElectTheDead <IElectTheDead@users.noreply.github.com>
2018-04-24 01:15:42 -0500

    Se agregaron las columnas al Entity TypeContent
    Se agregaron las columnas al Entity TypeContent.

--- src/main/java/com/arimdor/sharedNotesBE/entity/TypeContent.java
+++ src/main/java/com/arimdor/sharedNotesBE/entity/TypeContent.java
@@ -3,11 +3,13 @@ package com.arimdor.sharedNotesBE.entity;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.sql.Timestamp;
 
 @Entity
 @Table(name = "Type_Content")
@@ -17,6 +19,45 @@ public class TypeContent {
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
     @Column(updatable = false, nullable = false, unique = true)
     private String id;
-    
 
+    @Column(nullable = false)
+    @UpdateTimestamp
+    private Timestamp name;
+
+    @Column
+    @UpdateTimestamp
+    private Timestamp description;
+
+    public TypeContent() {
+    }
+
+    public TypeContent(String id, Timestamp name, Timestamp description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Timestamp getName() {
+        return name;
+    }
+
+    public void setName(Timestamp name) {
+        this.name = name;
+    }
+
+    public Timestamp getDescription() {
+        return description;
+    }
+
+    public void setDescription(Timestamp description) {
+        this.description = description;
+    }
 }
