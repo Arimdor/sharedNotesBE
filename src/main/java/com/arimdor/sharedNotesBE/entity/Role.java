a2ad44a61bf26ea308ddbf8e19efed1c17a238ed
IElectTheDead <IElectTheDead@users.noreply.github.com>
2018-04-23 23:23:34 -0500

    Se creó el entity de Role
    Se creó el entity de Role.

---
+++ src/main/java/com/arimdor/sharedNotesBE/entity/Role.java
@@ -0,0 +1,63 @@
+package com.arimdor.sharedNotesBE.entity;
+
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+@Entity
+@Table
+@JsonIdentityInfo(generator = ObjectIdGenerators.UUIDGenerator.class, property = "id")
+public class Role implements Serializable {
+
+    @Id
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(updatable = false, nullable = false, unique = true)
+    private String id;
+
+    @Column(nullable = false)
+    @UpdateTimestamp
+    private Timestamp name;
+
+    @Column
+    @UpdateTimestamp
+    private Timestamp description;
+
+    public Role() {
+    }
+
+    public Role(String id, Timestamp name, Timestamp description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Timestamp getName() {
+        return name;
+    }
+
+    public void setName(Timestamp name) {
+        this.name = name;
+    }
+
+    public Timestamp getDescription() {
+        return description;
+    }
+
+    public void setDescription(Timestamp description) {
+        this.description = description;
+    }
+}
