83c3cd06d4e0602c202e97a22e6e429f4c71e9c0
Arimdor <rodrigo.120894@gmail.com>
2018-04-23 22:51:13 -0500

    Se creo Book entity
    Se creo Book entity

---
+++ src/main/java/com/arimdor/sharedNotesBE/entity/Book.java
@@ -0,0 +1,85 @@
+package com.arimdor.sharedNotesBE.entity;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table
+@JsonIdentityInfo(generator = ObjectIdGenerators.UUIDGenerator.class, property = "id")
+public class Book {
+    @Id
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(updatable = false, nullable = false, unique = true)
+    private String id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, updatable = false)
+    private String create_by;
+
+    @Column
+    @UpdateTimestamp
+    private Timestamp updated_at;
+
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    private Timestamp created_at;
+
+    public Book() {
+    }
+
+    public Book(String id, String title, String create_by, Timestamp updated_at, Timestamp created_at) {
+        this.id = id;
+        this.title = title;
+        this.create_by = create_by;
+        this.updated_at = updated_at;
+        this.created_at = created_at;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCreate_by() {
+        return create_by;
+    }
+
+    public void setCreate_by(String create_by) {
+        this.create_by = create_by;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+}
